746796159452b11ea51bfa2b089f740125a757a4
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-17 23:29:35 +0600

    add firebase auth

---
+++ src/components/Pages/Login/Firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDwrMirSrY12PDKJmkbex1-J6D8mLL7H6Y",
+    authDomain: "lifecare-hospital-3295a.firebaseapp.com",
+    projectId: "lifecare-hospital-3295a",
+    storageBucket: "lifecare-hospital-3295a.appspot.com",
+    messagingSenderId: "1011497141982",
+    appId: "1:1011497141982:web:b9521850c167f7ecf3644f"
+};
+
+export default firebaseConfig;
